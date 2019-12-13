aebbf6dd498eaaef0670931ab4f4d13b09c5a9da
SvetlanaDorokhova <sveta_d@list.ru>
2019-12-13 10:43:45 +0100

    add Junit and versions

--- src/test/java/BulkProcessorTest.java
+++ src/test/java/BulkProcessorTest.java
@@ -1,2 +1,22 @@
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BulkProcessorTest {
+
+  private RestHighLevelClient client;
+
+  @Before
+  public void before() {
+    client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+  }
+
+  @Test
+  public void test() {
+//    BulkProcessor bulkProcessor = new BulkProcessor.Builder()
+  }
+
 }
